62d93133cebcfa2874077b2a086349d422a3b2bd
jtrecenti <julio.trecenti@gmail.com>
2018-05-10 20:46:18 -0300

    atualizacoes para willi

--- R/data.R
+++ R/data.R
@@ -43,3 +43,41 @@
 #' @source \url{http://inter01.tse.jus.br/pesqele-publico/app/pesquisa/listarEstatisticos.xhtml}
 "pesq_details"
 
+
+#' pesqEle tidy dataset
+#'
+#' A tidy dataset containing the results of elections polls.
+#'
+#' @format A data frame with 11078 rows and 8 variables:
+#' \describe{
+#'    \item{id_seq}{.}
+#'    \item{id_pesq}{.}
+#'    \item{id_muni}{.}
+#'    \item{info_uf}{.}
+#'    \item{info_muni}{.}
+#'    \item{info_election}{.}
+#'    \item{info_position}{.}
+#'    \item{comp_nm}{.}
+#'    \item{comp_cnpj}{.}
+#'    \item{comp_contract_same}{.}
+#'    \item{stat_id}{.}
+#'    \item{stat_nm}{.}
+#'    \item{pesq_n}{.}
+#'    \item{pesq_val}{.}
+#'    \item{pesq_contractors}{.}
+#'    \item{pesq_origin}{.}
+#'    \item{pesq_contractors}{.}
+#'    \item{pesq_origin}{.}
+#'    \item{dt_reg}{.}
+#'    \item{dt_pub}{.}
+#'    \item{dt_start}{.}
+#'    \item{dt_end}{.}
+#'    \item{txt_verif}{.}
+#'    \item{txt_method}{.}
+#'    \item{txt_about}{.}
+#'    \item{txt_plan}{.}
+#' }
+#' @source \url{http://inter01.tse.jus.br/pesqele-publico/app/pesquisa/listarEstatisticos.xhtml}
+"pesqEle"
+
+
