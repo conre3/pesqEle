78e5c3cee80e2363a59509610cf8b800281bf831
williamorim <thewilliam89@gmail.com>
2018-05-12 18:38:12 -0300

    Organizando arquivos

---
+++ inst/app/modules/geral/geral-barplot.R
@@ -0,0 +1,48 @@
+
+# Module UI ---------------------------------------------------------------
+
+barplotOutput <- function(id) {
+  ns <- NS(id)
+  
+  tags$div(
+    highcharter::highchartOutput(ns("plot"))  
+  )
+}
+
+# Module server -----------------------------------------------------------
+
+barplot <- function(input, output, session, df_pesq) {
+  
+  df <- reactive({
+    
+    df_pesq %>%
+      dplyr::mutate(
+        month = lubridate::month(dt_reg, label = TRUE),
+        abrangencia = ifelse(info_uf == "BRASIL", "nac", "est")
+      ) %>%
+      dplyr::count(abrangencia, month) %>% 
+      tidyr::spread(abrangencia, n) -> df_s
+  })
+  
+  output$plot <- highcharter::renderHighchart({
+  
+    df_s <- df()
+    
+    highcharter::highchart() %>%
+      highcharter::hc_chart(type = "column") %>% 
+      highcharter::hc_xAxis(categories = df_s$month) %>%
+      highcharter::hc_add_series(
+        df_s$est, 
+        name = "Pesquisas estaduais", 
+        showInLegend = TRUE,
+        color = "#3399ff") %>% 
+      highcharter::hc_add_series(
+        df_s$nac, 
+        name = "Pesquisas nacionais", 
+        showInLegend = TRUE,
+        color = "#ff7f50")
+    
+  })
+  
+}
+
