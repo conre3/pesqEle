6ac81f0ab76bad10ece040b189a0c0a23afb2b33
williamorim <thewilliam89@gmail.com>
2018-05-13 23:30:17 -0300

    arquivo utils.R

---
+++ inst/app/utils/utils.R
@@ -0,0 +1,16 @@
+real <- function(x) {
+  
+  stringr::str_c(
+    "R$", 
+    format(x, big.mark = ".", nsmall = 2, digits = 2, decimal.mark = ",")
+  )
+  
+}
+
+median_price <- function(x) {
+  
+  x %>% 
+    median(na.rm = TRUE) %>% 
+    round(2)
+  
+}
