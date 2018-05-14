0dca04c686e29cbf85c58f58cff6da9321b61561
williamorim <thewilliam89@gmail.com>
2018-05-13 23:31:25 -0300

    modulo "estatisticos"

--- inst/app/modules/estatisticos.R
+++ inst/app/modules/estatisticos.R
@@ -1,19 +1,54 @@
 
 # Modules -----------------------------------------------------------------
 
+source("modules/estatisticos/tableStat.R")
+source("modules/estatisticos/selectStat.R")
 
 # Module UI ---------------------------------------------------------------
 
-estatisticosUI <- function(id) {
+estatisticosUI <- function(id, df_pesq) {
   ns <- NS(id)
+  
+  tabItem(
+    tabName = id,
+    fluidRow(
+      tabBox(
+        width = 12,
+        tabItem(
+          tabName = "geral",
+          title = "Geral",
+          tableStatOutput(ns("table_stat")),
+          tags$hr(),
+          tags$p("* Valor mediano cobrado por pesquisa."),
+          tags$p("** Custo mediano por unidade amostral.")  
+        ),
+        tabItem(
+          tabName = "por_stat",
+          title = "Por estatístico",
+          selectStatInput("stat_select", df_pesq)
+          #statCountsOutput("stat_counts")
+        )
+      )
+    )
+  )
   
 }
 
 
 # Module server -----------------------------------------------------------
 
-estatisticos <- function(input, output, session) {
+estatisticos <- function(input, output, session, df_pesq) {
   
+  callModule(
+    module = tableStat,
+    id = "table_stat",
+    df_pesq = df_pesq
+  )
   
-  
+  df_pesq_stat <- callModule(
+    module = selectStat,
+    id = "stat_select",
+    df_pesq = df_pesq
+  )
+    
 }
