16bce00d6f9826a80b88945bbd166b1dad01a642
williamorim <thewilliam89@gmail.com>
2018-05-17 20:12:22 -0300

    Funções de formatação de texto

--- inst/app/utils/utils.R
+++ inst/app/utils/utils.R
@@ -1,4 +1,4 @@
-real_format <- function(x) {
+format_real <- function(x) {
   
   stringr::str_c(
     "R$", 
@@ -22,5 +22,30 @@ get_choices <- function(df, variable) {
     dplyr::distinct(x) %>% 
     dplyr::arrange(x) %>% 
     purrr::flatten_chr()
+  
+}
+
+hulk <- function(string) {
+  stringr::str_c("<strong>", string, "</strong>")
+}
+
+format_cnpj <- function(string) {
+  
+  stringr::str_c(
+    stringr::str_sub(string, 1, 2),
+    ".",
+    stringr::str_sub(string, 3, 5),
+    ".",
+    stringr::str_sub(string, 6, 8),
+    "/",
+    stringr::str_sub(string, 9, 12),
+    "-",
+    stringr::str_sub(string, 13, 14)
+  )
+}
+
+format_date <- function(string) {
+  
+  format(string, format = "%d de %B de %Y")
   
 }
